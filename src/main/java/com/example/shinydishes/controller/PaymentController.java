4c4277015abe6991092b3d3038c304bb1a534624
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:43:32 +0200

    Update PaymentController.java

--- src/main/java/com/example/shinydishes/controller/PaymentController.java
+++ src/main/java/com/example/shinydishes/controller/PaymentController.java
@@ -1,2 +1,37 @@
-package com.example.shinydishes.controller;public class PaymentController {
+package com.example.shinydishes.controller;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class PaymentController {
+
+    @Value("${stripe.secret.key}")
+    private String stripeSecretKey;
+
+    @PostMapping("/charge")
+    public String charge(@RequestParam String stripeToken, @RequestParam int totalAmount) {
+        Stripe.apiKey = stripeSecretKey;
+
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", totalAmount);
+        chargeParams.put("currency", "usd");
+        chargeParams.put("description", "Example charge");
+        chargeParams.put("source", stripeToken);
+
+        try {
+            Charge charge = Charge.create(chargeParams);
+            return "Успішний платіж";
+        } catch (StripeException e) {
+            return "Помилка під час виконання платежу: " + e.getMessage();
+        }
+    }
 }
