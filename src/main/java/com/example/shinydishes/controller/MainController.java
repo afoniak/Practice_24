c997dc2590724553f951a89812d4ff5ed8b0283d
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:42:14 +0200

    Update MainController.java

--- src/main/java/com/example/shinydishes/controller/MainController.java
+++ src/main/java/com/example/shinydishes/controller/MainController.java
@@ -1,2 +1,20 @@
-package com.example.shinydishes.controller;public class MainController {
+package com.example.shinydishes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home (Model model) {
+        return "home";
+    }
+
+    @GetMapping("/home")
+    public String homepage (Model model) {
+        return "home";
+    }
+
 }
