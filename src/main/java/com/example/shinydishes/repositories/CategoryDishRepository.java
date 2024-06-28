30b03714da805e71a0006f0581d94e4b69030235
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:46:09 +0200

    Update CategoryDishRepository.java

--- src/main/java/com/example/shinydishes/repositories/CategoryDishRepository.java
+++ src/main/java/com/example/shinydishes/repositories/CategoryDishRepository.java
@@ -1,2 +1,11 @@
-package com.example.shinydishes.repositories;public interface CategoryDishRepository {
+package com.example.shinydishes.repositories;
+
+import com.example.shinydishes.models.CategoryDish;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryDishRepository extends CrudRepository<CategoryDish, Long> {
+    List<CategoryDish> findAll();
+    List<CategoryDish> findByType(String type);
 }
