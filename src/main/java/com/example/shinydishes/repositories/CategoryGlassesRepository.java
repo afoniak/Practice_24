62d38d7459647f24dcd32ecb2f875cad4afd4ea6
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:46:32 +0200

    Update CategoryGlassesRepository.java

--- src/main/java/com/example/shinydishes/repositories/CategoryGlassesRepository.java
+++ src/main/java/com/example/shinydishes/repositories/CategoryGlassesRepository.java
@@ -1,2 +1,11 @@
-package com.example.shinydishes.repositories;public interface CategoryGlassesRepository {
+package com.example.shinydishes.repositories;
+
+import com.example.shinydishes.models.CategoryGlasses;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryGlassesRepository extends CrudRepository<CategoryGlasses, Long> {
+    List<CategoryGlasses> findByType(String type);
 }
+
