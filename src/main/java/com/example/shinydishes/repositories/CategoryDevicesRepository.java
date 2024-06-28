c929c4a2ed589a42dcfa5a996af2bc3758ec1491
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:45:58 +0200

    Update CategoryDevicesRepository.java

--- src/main/java/com/example/shinydishes/repositories/CategoryDevicesRepository.java
+++ src/main/java/com/example/shinydishes/repositories/CategoryDevicesRepository.java
@@ -1,2 +1,10 @@
-package com.example.shinydishes.repositories;public interface CategoryDevicesRepository {
+package com.example.shinydishes.repositories;
+
+import com.example.shinydishes.models.CategoryDevices;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryDevicesRepository extends CrudRepository<CategoryDevices, Long> {
+    List<CategoryDevices> findByType(String type);
 }
