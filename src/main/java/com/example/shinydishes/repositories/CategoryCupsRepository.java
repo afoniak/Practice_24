bbc0f476030b35e08d486b05f21490bdaf2e4fd2
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:45:47 +0200

    Update CategoryCupsRepository.java

--- src/main/java/com/example/shinydishes/repositories/CategoryCupsRepository.java
+++ src/main/java/com/example/shinydishes/repositories/CategoryCupsRepository.java
@@ -1,2 +1,10 @@
-package com.example.shinydishes.repositories;public interface CategoryCupsRepository {
+package com.example.shinydishes.repositories;
+
+import com.example.shinydishes.models.CategoryCups;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryCupsRepository extends CrudRepository<CategoryCups, Long> {
+    List<CategoryCups> findByType(String type);
 }
