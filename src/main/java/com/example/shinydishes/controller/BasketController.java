a93aaec20c78f176ead11f8cbe96641c88a3c333
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:40:51 +0200

    Update BasketController.java

--- src/main/java/com/example/shinydishes/controller/BasketController.java
+++ src/main/java/com/example/shinydishes/controller/BasketController.java
@@ -1,2 +1,27 @@
-package com.example.shinydishes.controller;public class BasketController {
+package com.example.shinydishes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpSession;
+
+
+
+@Controller
+public class BasketController {
+
+    @GetMapping("/basket")
+    public String basket(Model model, HttpSession session) {
+        return "basket";
+    }
+
+    @GetMapping("/payment")
+    public String payment(Model model, HttpSession session) {
+        return "payment";
+    }
+
+    @GetMapping("/selected")
+    public String selected(Model model, HttpSession session) {
+        return "selected";
+    }
 }
