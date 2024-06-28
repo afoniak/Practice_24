a312bfe7cf4ad90872a454f3519386aa68db6668
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:44:58 +0200

    Update CategoryPots.java

--- src/main/java/com/example/shinydishes/models/CategoryPots.java
+++ src/main/java/com/example/shinydishes/models/CategoryPots.java
@@ -1,2 +1,91 @@
-package com.example.shinydishes.models;public class CategoryPots {
+package com.example.shinydishes.models;
+
+import jakarta.persistence.*;
+import org.hibernate.validator.constraints.Length;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "category_glasses")
+public class CategoryGlasses {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String img;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Length(max = 500)
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
+
