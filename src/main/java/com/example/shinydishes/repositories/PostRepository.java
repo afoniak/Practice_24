5d32b5114c198129bb81e390348ae79100cc4ec8
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:47:00 +0200

    Update PostRepository.java

--- src/main/java/com/example/shinydishes/repositories/PostRepository.java
+++ src/main/java/com/example/shinydishes/repositories/PostRepository.java
@@ -1,2 +1,7 @@
-package com.example.shinydishes.repositories;public interface PostRepository {
+package com.example.shinydishes.repositories;
+
+import com.example.shinydishes.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post,Long> {
 }
