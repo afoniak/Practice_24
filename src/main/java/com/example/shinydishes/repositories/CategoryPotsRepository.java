f62341248eb6b229c2402343c208b574de3a916b
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:46:42 +0200

    Update CategoryPotsRepository.java

--- src/main/java/com/example/shinydishes/repositories/CategoryPotsRepository.java
+++ src/main/java/com/example/shinydishes/repositories/CategoryPotsRepository.java
@@ -1,2 +1,10 @@
-package com.example.shinydishes.repositories;public interface CategoryPotsrepository {
+package com.example.shinydishes.repositories;
+
+import com.example.shinydishes.models.CategoryPots;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryPotsRepository extends CrudRepository<CategoryPots, Long> {
+    List<CategoryPots> findByType(String type);
 }
