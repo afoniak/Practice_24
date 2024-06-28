3ee5a4a2ea9e4b39a5028984f2c6097d480ff60c
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:46:19 +0200

    Update CategoryFryingPanRepository.java

--- src/main/java/com/example/shinydishes/repositories/CategoryFryingPanRepository.java
+++ src/main/java/com/example/shinydishes/repositories/CategoryFryingPanRepository.java
@@ -1,2 +1,11 @@
-package com.example.shinydishes.repositories;public interface CategoryFryingPanRepository {
+package com.example.shinydishes.repositories;
+
+import com.example.shinydishes.models.CategoryFryingPan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CategoryFryingPanRepository extends CrudRepository<CategoryFryingPan, Long> {
+    List<CategoryFryingPan> findByType(String type);
 }
+
