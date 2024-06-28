c9a6bc6e1035a0a14dafe3c38d05c3ba7b8f67fb
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:41:16 +0200

    Update ContactController.java

--- src/main/java/com/example/shinydishes/controller/ContactController.java
+++ src/main/java/com/example/shinydishes/controller/ContactController.java
@@ -1,2 +1,29 @@
-package com.example.shinydishes.controller;public class ContactController {
+package com.example.shinydishes.controller;
+
+import com.example.shinydishes.models.Post;
+import com.example.shinydishes.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ContactController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public String contactAdd (@RequestParam String name, @RequestParam String email, @RequestParam String message, Model model) {
+        Post post = new Post(name,email,message);
+        postRepository.save(post);
+        return "contact";
+    }
 }
