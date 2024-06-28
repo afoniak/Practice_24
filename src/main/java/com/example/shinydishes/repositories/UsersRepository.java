076f8f94361ff4fe92fe7080a0bc94a5be1a60d6
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:47:11 +0200

    Update UsersRepository.java

--- src/main/java/com/example/shinydishes/repositories/UsersRepository.java
+++ src/main/java/com/example/shinydishes/repositories/UsersRepository.java
@@ -1,2 +1,10 @@
-package com.example.shinydishes.repositories;public interface UsersRepository {
+package com.example.shinydishes.repositories;
+
+import com.example.shinydishes.models.Users;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<Users, Long> {
+    boolean existsByLogin(String login);
+    boolean existsByEmail(String email);
+    Users findByEmail(String email);
 }
