654d1a6481be31843809414de1f1bac95c9ea120
Yevhenii <127236282+afoniak@users.noreply.github.com>
2024-06-28 04:45:09 +0200

    Update Post.java

--- src/main/java/com/example/shinydishes/models/Post.java
+++ src/main/java/com/example/shinydishes/models/Post.java
@@ -1,2 +1,78 @@
-package com.example.shinydishes.models;public class Post {
+package com.example.shinydishes.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import java.time.LocalDateTime;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Post(){
+
+    }
+    public Post(String name,String email,String message){
+        this.name = name;
+        this.email = email;
+        this.message = message;
+    }
 }
